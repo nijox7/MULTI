d326f76c6ffd1d50c70c3d57286c6fee8879d53e
nijox7 <clement.carbou@gmail.com>
2026-04-18 18:16:11 +0200

    tp7 a continuer

--- TP7/soft/main_dma.c
+++ TP7/soft/main_dma.c
@@ -28,9 +28,10 @@ __attribute__ ((constructor)) void main() {
             tty_printf("\n!!! error in fb_syn_write syscall !!!\n"); 
             // exit();
         }
+
 	    if(fb_completed()) { // wait until fb_write() finished
             tty_printf("\n!!! error in fb_completed syscall !!!\n");
-            exit();
+//            exit();
         };
 
         tty_printf(" - display OK at cycle %d\n", proctime());
